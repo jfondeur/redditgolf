ab1660169687e15b03b57b7aa5dcbf16664af2c4
Jean Fondeur <jean@fondeurs.com>
2023-06-12 18:20:18 -0400

    Landing page push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'heading': ['Montserrat', 'sans-serif'  ]
+      }
+    },
+  },
+  plugins: [],
+}
+
